daa19f54ea4076228dc50370e6a6629888ec2daa
DESKTOP-NITA170\Pili <pilarmosto@gmail.com>
2023-10-29 13:29:11 -0300

    objeto usuario se guarda en variable session

---
+++ TPINT_GRUPO_2_LAB4/src/dao/UsuarioDao.java
@@ -0,0 +1,9 @@
+package dao;
+
+import entidades.Usuario;
+
+public interface UsuarioDao {
+	
+	
+	public Usuario getUsuarioxUser (String usuario);
+}
---
+++ TPINT_GRUPO_2_LAB4/src/daoImpl/UsuarioDaoImpl.java
@@ -0,0 +1,66 @@
+package daoImpl;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import dao.UsuarioDao;
+import entidades.Cliente;
+import entidades.Usuario;
+
+public class UsuarioDaoImpl implements UsuarioDao{
+	
+	private static final String readall = "SELECT * FROM usuarios";
+	
+	
+	@Override
+	public Usuario getUsuarioxUser(String usuario) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	
+		Usuario u = new Usuario();
+		Conexion conexion = Conexion.getConexion();
+		try{
+			
+			PreparedStatement statement = conexion.getSQLConexion().prepareStatement(readall);
+	        ResultSet resultSet = statement.executeQuery();
+		
+			while(resultSet.next()){
+				
+				if(resultSet.getString("id_usuario").compareTo(usuario) == 0) {
+					
+					
+					//clases
+					Cliente cliente = new Cliente();
+				
+					
+					u.setId(resultSet.getString("id_usuario"));
+					cliente.setDNI(resultSet.getString("DNI"));
+					u.setDni(cliente);
+					u.setEsAdmin(resultSet.getBoolean("esAdmin"));
+					u.setIdRef(resultSet.getInt("id_ref"));
+					u.setContraseña(resultSet.getString("contraseña"));
+					u.setNombreUsuario(resultSet.getString("nombre_usuario"));
+					u.setEstado(resultSet.getBoolean("estado"));
+
+				
+					
+					return u;
+					
+				}
+				
+			}
+
+		conexion.cerrarConexion();
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return u;
+	}
+}
