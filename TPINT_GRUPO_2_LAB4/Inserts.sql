8d8fe9b8f339be1cd89cadb96657e042bd590f96
Zoe Rouco <zoerouco@gmail.com>
2023-11-12 15:28:57 -0300

    fix inserts

--- TPINT_GRUPO_2_LAB4/Inserts.sql
+++ TPINT_GRUPO_2_LAB4/Inserts.sql
@@ -17,17 +17,8 @@ SELECT 'transferencia_recibida','Genera un movimiento de dinero positivo en la c
 
 INSERT INTO clientes(DNI, id_genero, id_nacionalidad, id_provincia, id_localidades, CUIL, nombre, apellido, fecha_nacimiento, direccion, 
 						correo_electronico, telefono_primario, telefono_secundario, estado)
-SELECT '40789452', 'NULL', '1', '1', '1', '23407894523', 'admin', NULL, NULL, 'globank 0101', 'support_globank@gmail.com', '1105263398', NULL, 1 UNION
+SELECT '40789452', 'NULL', '1', '1', '1', '23407894523', 'admin_banco', 'ramirez', NULL, 'globank 0101', 'support_globank@gmail.com', '1105263398', NULL, 1 UNION
 SELECT '45879526', 'M', '1', '1', '1', '23458795263', 'Natalia', 'Gomez', '2000/08/03', 'velez 189', 'natigomez@gmail.com', '1189758630', '1178452033', 1;
 
-INSERT INTO usuarios(id_usuario,DNI, esAdmin, id_ref, contraseña, nombre_usuario, estado)
-SELECT '00','45879526', 1, 1, 'globankroot', 'admin_banco', 1; 
-
-INSERT INTO usuarios(id_usuario,DNI, esAdmin, id_ref, contraseña, nombre_usuario, estado)
-SELECT '2','45879526', 1, 1, 'globankuser', 'user', 1; 
-
-INSERT INTO usuarios(id_usuario,DNI, esAdmin, id_ref, contraseña, nombre_usuario, estado)
-SELECT '3','45879526', 0, 1, 'user', 'user1', 1; 
-
-
-Select * from bdglobank.usuarios;
+INSERT INTO usuarios(DNI, esAdmin, id_ref, contraseña, nombre_usuario, estado)
+SELECT '45879526', 1, 1, 'globankroot', 'admin_banco', 1;
