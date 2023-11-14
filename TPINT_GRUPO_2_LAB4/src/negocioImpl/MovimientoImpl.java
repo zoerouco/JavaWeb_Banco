204ebfce21fd807a4bf6203ee74a6f28238d5e38
Luciano Vitale <lucianovitale00@gmail.com>
2023-11-13 22:58:48 -0300

    Fix último movimiento de la cuenta

--- TPINT_GRUPO_2_LAB4/src/negocioImpl/MovimientoImpl.java
+++ TPINT_GRUPO_2_LAB4/src/negocioImpl/MovimientoImpl.java
@@ -9,11 +9,10 @@ import entidades.Movimiento;
 import entidades.Prestamo;
 import negocio.MovimientoNegocio;
 
-
 public class MovimientoImpl implements MovimientoNegocio {
-	
-MovimientoDao mdao = new MovimientoDaoImpl();
-	
+
+	MovimientoDao mdao = new MovimientoDaoImpl();
+
 	@Override
 	public boolean insert(Movimiento movimiento) {
 		boolean filas = false;
@@ -27,18 +26,16 @@ MovimientoDao mdao = new MovimientoDaoImpl();
 		return false;
 	}
 
+	/*
+	 * public ArrayList<Movimiento> readAll() { return mdao.readAll(); }
+	 */
 
-	/*public ArrayList<Movimiento> readAll() {
-		return mdao.readAll();
-	}*/
+	public ArrayList<Movimiento> getMovimientosXCuenta(Cuenta cuenta) {
 
-	public ArrayList<Movimiento> getMovimientosXCuenta (Cuenta cuenta) {
-		
-		
 		return mdao.getMovimientosXCuenta(cuenta);
-		
+
 	}
-	
+
 	@Override
 	public boolean update(Movimiento movimiento) {
 		// TODO Auto-generated method stub
@@ -49,29 +46,14 @@ MovimientoDao mdao = new MovimientoDaoImpl();
 		// TODO Auto-generated method stub
 		return mdao.getUltimoID();
 	}
-	
-	public Movimiento getUltimoMovimientoCuenta (Cuenta cuenta) {
-		
-	ArrayList<Movimiento> movimientosCliente = this.getMovimientosXCuenta(cuenta);
-	Movimiento movimientoMayor = new Movimiento();
-		
-        if (movimientosCliente.size() > 0) {
-           
-            int mayorId = movimientosCliente.get(0).getId_movimiento();
-            
-            for (Movimiento movimiento : movimientosCliente) {
-                if (movimiento.getId_movimiento() > mayorId) {
-                    mayorId = movimiento.getId_movimiento();
-                    movimientoMayor = movimiento;           
-                }
-            }
-        }
-		
-		return movimientoMayor;
-		
-		
+
+	public Movimiento getUltimoMovimientoCuenta(Cuenta cuenta) {
+		ArrayList<Movimiento> movimientosCliente = this.getMovimientosXCuenta(cuenta);
+		if (movimientosCliente.size() > 0) {
+			return movimientosCliente.get(0);
+		}
+
+		return null;
 	}
-	
-	
-	
+
 }
