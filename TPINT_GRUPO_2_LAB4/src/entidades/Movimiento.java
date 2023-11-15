1678231b5e4c1024c20d9d4f6c58aff09cb44d56
Zoe Rouco <zoerouco@gmail.com>
2023-11-15 18:00:22 -0300

    Vuelta atras a

--- TPINT_GRUPO_2_LAB4/src/entidades/Movimiento.java
+++ TPINT_GRUPO_2_LAB4/src/entidades/Movimiento.java
@@ -14,22 +14,19 @@ public class Movimiento {
 	private int id_movimiento;
 	private Cuenta CBU;	
 	private Cuenta CBU_Destino;
-	private LocalDateTime Fecha_Transaccion;
+	private Date Fecha_Transaccion;
 	private float Importe;
 	private String Detalle;	
 	private Tipo_Movimiento TipoMovimiento;
 	private boolean estado;
-	
-	 
-	
-	
-	
+
 	// Constructor para Id_Mov
 	public Movimiento () {
 		ID_Movimiento++;
 	}
-	
-	public Movimiento(int id_movimiento, Cuenta cBU, Cuenta cBU_Destino, LocalDateTime fecha_Transaccion, float importe, String detalle, Tipo_Movimiento tipoMovimiento, boolean estado) {
+
+	public Movimiento(int id_movimiento, Cuenta cBU, Cuenta cBU_Destino, Date fecha_Transaccion, float importe,
+			String detalle, Tipo_Movimiento tipoMovimiento, boolean estado) {
 		super();
 		this.id_movimiento = id_movimiento;
 		CBU = cBU;
@@ -41,77 +38,71 @@ public class Movimiento {
 		this.estado = estado;
 	}
 
-		public int getId_movimiento() {
-			return id_movimiento;
-		}
+	public int getId_movimiento() {
+		return id_movimiento;
+	}
 
+	public void setId_movimiento(int id_movimiento) {
+		this.id_movimiento = id_movimiento;
+	}
 
-		public void setId_movimiento(int id_movimiento) {
-			this.id_movimiento = id_movimiento;
-		}
-	
-		
-		public Cuenta getCBU() {
-			return CBU;
-		}
-		
-		public void setCBU(Cuenta CBU) {
-			this.CBU = CBU;
-		}
-		
-		public Cuenta getCBU_Destino() {
-			return CBU_Destino;
-		}
-		
-		public void setCBU_Destino(Cuenta CBU_Destino) {
-			this.CBU_Destino = CBU_Destino;
-		}
-		
-		public LocalDateTime getFecha_Transaccion() {
-			return Fecha_Transaccion;
-		}
+	public Cuenta getCBU() {
+		return CBU;
+	}
 
-		public void setFecha_Transaccion(LocalDateTime fecha_Transaccion) {
-			Fecha_Transaccion = fecha_Transaccion;
-		}
+	public void setCBU(Cuenta cBU) {
+		CBU = cBU;
+	}
 
-		public float getImporte() {
-			return Importe;
-		}
-		
-		public void setImporte(float Importe) {
-			this.Importe = Importe;
-		}
-		
-		public String getDetalle() {
-			return Detalle;
-		}
-		
-		public void setDetalle(String Detalle) {
-			this.Detalle = Detalle;
-		}
-		
-		public Tipo_Movimiento getTipoMovimiento() {
-			return TipoMovimiento;
-		}
-		
-		public void setTipoMovimiento(Tipo_Movimiento TipoMovimiento) {
-			this.TipoMovimiento = TipoMovimiento;
-		}
-		
-		public boolean getEstado() {
-			return estado;
-		}
-		
-		public void setEstado(boolean estado) {
-			this.estado = estado;
-		}
-		
-		public static int getID_Movimiento() {
-			return ID_Movimiento;
-		}
-		
-		public int getID_MovimientoInstancia() {
-			return this.id_movimiento;
-		}
+	public Cuenta getCBU_Destino() {
+		return CBU_Destino;
+	}
+
+	public void setCBU_Destino(Cuenta cBU_Destino) {
+		CBU_Destino = cBU_Destino;
+	}
+
+	public Date getFecha_Transaccion() {
+		return Fecha_Transaccion;
+	}
+
+	public void setFecha_Transaccion(Date fecha_Transaccion) {
+		Fecha_Transaccion = fecha_Transaccion;
+	}
+
+	public float getImporte() {
+		return Importe;
+	}
+
+	public void setImporte(float importe) {
+		Importe = importe;
+	}
+
+	public String getDetalle() {
+		return Detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		Detalle = detalle;
+	}
+
+	public Tipo_Movimiento getTipoMovimiento() {
+		return TipoMovimiento;
+	}
+
+	public void setTipoMovimiento(Tipo_Movimiento tipoMovimiento) {
+		TipoMovimiento = tipoMovimiento;
+	}
+
+	public boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public static int getID_Movimiento() {
+		return ID_Movimiento;
+	}	
 }
